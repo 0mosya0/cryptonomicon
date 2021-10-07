d5bc1ec4f60580bc204c7788bdc2e1d90d3b66e7
Елисеенко Илья <eliseenko_i@ipps.by>
2021-10-07 10:47:17 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -198,10 +198,30 @@ export default {
 
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdates(ticker.name);
+      });
     }
   },
 
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=189c035f75988d198428cf06a5aa566d32e637fd386b44e879707738648deefe`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name === tickerName).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 5000);
+
+      this.ticker = "";
+    },
+
     add() {
       const currentTicker = {
         name: this.ticker,
@@ -211,20 +231,7 @@ export default {
       this.tickers.push(currentTicker);
 
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
-
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=189c035f75988d198428cf06a5aa566d32e637fd386b44e879707738648deefe`
-        );
-        const data = await f.json();
-        this.tickers.find((t) => t.name === currentTicker.name).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 5000);
-      this.ticker = "";
+      this.subscribeToUpdates(currentTicker.name);
     },
 
     handleDelete(tickerToRemove) {
